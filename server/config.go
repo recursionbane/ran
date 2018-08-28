512b1c33255e8a535ced3821a92d4663d2989bc4
recursionbane <recursionbane@gmail.com>
2018-08-28 16:10:47 -0700

    Support for -listdirasjson

--- server/config.go
+++ server/config.go
@@ -36,20 +36,21 @@ type Auth struct {
 
 
 type Config struct {
-    Root        string          // Root path of the website. Default is current working directory.
-    Path404     *string         // Abspath of custom 404 file, under directory of Root.
-                                // When a 404 not found error occurs, the file's content will be send to client.
-                                // nil means do not use 404 file.
-    Path401     *string         // Abspath of custom 401 file, under directory of Root.
-                                // When a 401 unauthorized error occurs, the file's content will be send to client.
-                                // nil means do not use 401 file.
-    IndexName   Index           // File name of index, priority depends on the order of values.
-                                // Default is []string{"index.html", "index.htm"}.
-    ListDir     bool            // If no index file provide, show file list of the directory.
-                                // Default is false.
-    Gzip        bool            // If turn on gzip compression, default is true.
-    Auth        *Auth           // If not nil, turn on authentication.
-    ServeAll    bool            // If is false, path start with dot will not be served, that means a 404 error will be returned.
+    Root            string          // Root path of the website. Default is current working directory.
+    Path404         *string         // Abspath of custom 404 file, under directory of Root.
+                                    // When a 404 not found error occurs, the file's content will be send to client.
+                                    // nil means do not use 404 file.
+    Path401         *string         // Abspath of custom 401 file, under directory of Root.
+                                    // When a 401 unauthorized error occurs, the file's content will be send to client.
+                                    // nil means do not use 401 file.
+    IndexName       Index           // File name of index, priority depends on the order of values.
+                                    // Default is []string{"index.html", "index.htm"}.
+    ListDir         bool            // If no index file provide, show file list of the directory.
+                                    // Default is false.
+    ListDirAsJSON   bool            // Directory listing should be returned as JSON
+    Gzip            bool            // If turn on gzip compression, default is true.
+    Auth            *Auth           // If not nil, turn on authentication.
+    ServeAll        bool            // If is false, path start with dot will not be served, that means a 404 error will be returned.
 }
 
 
