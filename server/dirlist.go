1ae1c49613a270acd34d7f288acab274c8538b02
recursionbane <recursionbane@gmail.com>
2018-09-06 14:07:07 -0700

    Added support for --ignorefileext; gofmt

--- server/dirlist.go
+++ server/dirlist.go
@@ -14,20 +14,18 @@ import "strings"
 import "encoding/json"
 
 type dirListFiles struct {
-    Name        string
-    Url         string
-    Size        int64
-    ModTime     time.Time
-    IsDir       bool
-    Mode        os.FileMode
+	Name    string
+	Url     string
+	Size    int64
+	ModTime time.Time
+	IsDir   bool
+	Mode    os.FileMode
 }
-
 
 type dirList struct {
-    Title string
-    Files []dirListFiles
+	Title string
+	Files []dirListFiles
 }
-
 
 const dirListTpl = `<!DOCTYPE HTML>
 <html>
@@ -90,119 +88,134 @@ table tr > td:nth-child(2), table tr > td:nth-child(3) {
 </body>
 </html>`
 
-
 var tplDirList *template.Template
 
-
 func timeToString(t time.Time, format ...string) string {
-    f := "2006-01-02 15:04:05"
-    if len(format) > 0 && format[0] != "" {
-        f = format[0]
-    }
-    return t.Format(f)
+	f := "2006-01-02 15:04:05"
+	if len(format) > 0 && format[0] != "" {
+		f = format[0]
+	}
+	return t.Format(f)
 }
-
 
 func init() {
-    var err error
-    tplDirList = template.New("dirlist").Funcs(template.FuncMap{"t2s": timeToString})
-    tplDirList, err = tplDirList.Parse(dirListTpl)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Directory list template init error: %s", err.Error())
-        os.Exit(1)
-    }
+	var err error
+	tplDirList = template.New("dirlist").Funcs(template.FuncMap{"t2s": timeToString})
+	tplDirList, err = tplDirList.Parse(dirListTpl)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Directory list template init error: %s", err.Error())
+		os.Exit(1)
+	}
 }
 
+// Check if a string element is in a list
+func isStringInStringList(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+// Check if a string element is in a CSV string (implict conversion to a list)
+func isStringInCSV(a string, rawlist string) bool {
+	// Convert the rawlist into a list by splitting at commas
+	return isStringInStringList(a, strings.Split(rawlist, ","))
+}
 
 // List content of a directory.
 // If error occurs, this function will return an error and won't write anything to ResponseWriter.
 func (this *RanServer) listDir(w http.ResponseWriter, serveAll bool, c *context) (size int64, err error) {
 
-    if !c.exist {
-        size = Error(w, 404)
-        return
-    }
+	if !c.exist {
+		size = Error(w, 404)
+		return
+	}
 
-    if !c.isDir {
-        err = fmt.Errorf("Cannot list contents of a non-directory")
-        return
-    }
+	if !c.isDir {
+		err = fmt.Errorf("Cannot list contents of a non-directory")
+		return
+	}
 
-    f, err := os.Open(c.absFilePath)
-    if err != nil {
-        return
-    }
-    defer f.Close()
+	f, err := os.Open(c.absFilePath)
+	if err != nil {
+		return
+	}
+	defer f.Close()
 
-    info, err := f.Readdir(0)
-    if err != nil {
-        return
-    }
+	info, err := f.Readdir(0)
+	if err != nil {
+		return
+	}
 
-    // Override below if listdirasjson was set
-    w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	// Override below if listdirasjson was set
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-    title := html.EscapeString(path.Base(c.cleanPath))
+	title := html.EscapeString(path.Base(c.cleanPath))
 
-    var files []dirListFiles
+	var files []dirListFiles
 
-    for n, i := range info {
-        name := i.Name()
-        if i.IsDir() {
-            name += "/"
-        }
-        name = html.EscapeString(name)
+	for n, i := range info {
+		name := i.Name()
+		if i.IsDir() {
+			name += "/"
+		}
 
-        // skip hidden path
-        if !serveAll && strings.HasPrefix(name, ".") {
-            continue
-        }
+		// Check if the extension of this file is in the ignore list
+		ext := filepath.Ext(name)
+		if isStringInCSV(ext, c.ignorefileext) {
+			continue
+		}
 
-        fileUrl:= url.URL{Path: name}
+		name = html.EscapeString(name)
 
-        // write parent dir
-        if n == 0 && c.cleanPath != "/" {
-            parent := c.parent()
+		// skip hidden path
+		if !serveAll && strings.HasPrefix(name, ".") {
+			continue
+		}
 
-            // unescape parent before get it's modification time
-            var parentUnescape string
-            parentUnescape, err = url.QueryUnescape(parent)
-            if err != nil {
-                return
-            }
+		fileUrl := url.URL{Path: name}
 
-            var info os.FileInfo
-            info, err = os.Stat(filepath.Join(this.config.Root, parentUnescape))
-            if err != nil {
-                return
-            }
+		// write parent dir
+		if n == 0 && c.cleanPath != "/" {
+			parent := c.parent()
 
-            files = append(files, dirListFiles{Name:"[..]", Url:parent, ModTime:info.ModTime(), IsDir:true})
-        }
+			// unescape parent before get it's modification time
+			var parentUnescape string
+			parentUnescape, err = url.QueryUnescape(parent)
+			if err != nil {
+				return
+			}
 
-        files = append(files, dirListFiles{Name:name, Url:fileUrl.String(), Size:i.Size(), ModTime:i.ModTime(), IsDir:i.IsDir(), Mode:i.Mode()})
-    }
+			var info os.FileInfo
+			info, err = os.Stat(filepath.Join(this.config.Root, parentUnescape))
+			if err != nil {
+				return
+			}
 
-    data := dirList{ Title: title, Files: files}
+			files = append(files, dirListFiles{Name: "[..]", Url: parent, ModTime: info.ModTime(), IsDir: true})
+		}
 
+		files = append(files, dirListFiles{Name: name, Url: fileUrl.String(), Size: i.Size(), ModTime: i.ModTime(), IsDir: i.IsDir(), Mode: i.Mode()})
+	}
 
-    buf := bufferPool.Get()
-    defer bufferPool.Put(buf)
+	data := dirList{Title: title, Files: files}
 
+	buf := bufferPool.Get()
+	defer bufferPool.Put(buf)
 
-    if c.listDirAsJSON {
-        // Return entities as JSON structure
-        w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	if c.listDirAsJSON {
+		// Return entities as JSON structure
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 
-        encoded_json, _ := json.Marshal(data)
-        io.WriteString(buf, string(encoded_json))
-    } else {
-        // Return HTML table
-        tplDirList.Execute(buf, data)
-    }
+		encoded_json, _ := json.Marshal(data)
+		io.WriteString(buf, string(encoded_json))
+	} else {
+		// Return HTML table
+		tplDirList.Execute(buf, data)
+	}
 
-    size, _ = buf.WriteTo(w)
-    return
+	size, _ = buf.WriteTo(w)
+	return
 }
-
-
