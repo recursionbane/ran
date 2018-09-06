3e1fe34ea85f3ebb086af3d8d719d28a082dd417
recursionbane <recursionbane@gmail.com>
2018-09-06 14:06:24 -0700

    Added support for --ignorefileext; gofmt

--- server/config.go
+++ server/config.go
@@ -2,55 +2,49 @@ package server
 
 import "strings"
 
-
 type Index []string
 
-
 func (this *Index) String() string {
-    return strings.Join([]string(*this), ":")
+	return strings.Join([]string(*this), ":")
 }
-
 
 func (this *Index) Set(value string) error {
-    *this = Index(strings.Split(value, ":"))
-    return nil
+	*this = Index(strings.Split(value, ":"))
+	return nil
 }
-
 
 type AuthMethod string
-const BasicMethod  AuthMethod = "basic"
+
+const BasicMethod AuthMethod = "basic"
 const DigestMethod AuthMethod = "digest"
 
-
 type Auth struct {
-    Username string
-    Password string
+	Username string
+	Password string
 
-    // paths which use password to protect, relative to "/".
-    // if Paths is empty, all paths are protected.
-    // not used currently
-    Paths    []string
+	// paths which use password to protect, relative to "/".
+	// if Paths is empty, all paths are protected.
+	// not used currently
+	Paths []string
 
-    Method AuthMethod
+	Method AuthMethod
 }
-
 
 type Config struct {
-    Root            string          // Root path of the website. Default is current working directory.
-    Path404         *string         // Abspath of custom 404 file, under directory of Root.
-                                    // When a 404 not found error occurs, the file's content will be send to client.
-                                    // nil means do not use 404 file.
-    Path401         *string         // Abspath of custom 401 file, under directory of Root.
-                                    // When a 401 unauthorized error occurs, the file's content will be send to client.
-                                    // nil means do not use 401 file.
-    IndexName       Index           // File name of index, priority depends on the order of values.
-                                    // Default is []string{"index.html", "index.htm"}.
-    ListDir         bool            // If no index file provide, show file list of the directory.
-                                    // Default is false.
-    ListDirAsJSON   bool            // Directory listing should be returned as JSON
-    Gzip            bool            // If turn on gzip compression, default is true.
-    Auth            *Auth           // If not nil, turn on authentication.
-    ServeAll        bool            // If is false, path start with dot will not be served, that means a 404 error will be returned.
+	Root    string  // Root path of the website. Default is current working directory.
+	Path404 *string // Abspath of custom 404 file, under directory of Root.
+	// When a 404 not found error occurs, the file's content will be send to client.
+	// nil means do not use 404 file.
+	Path401 *string // Abspath of custom 401 file, under directory of Root.
+	// When a 401 unauthorized error occurs, the file's content will be send to client.
+	// nil means do not use 401 file.
+	IndexName Index // File name of index, priority depends on the order of values.
+	// Default is []string{"index.html", "index.htm"}.
+	ListDir bool // If no index file provide, show file list of the directory.
+	// Default is false.
+	ListDirAsJSON bool   // Directory listing should be returned as JSON
+	IgnoreFileExt string // Extensions to ignore (CSV)
+	Gzip          bool   // If turn on gzip compression, default is true.
+	Auth          *Auth  // If not nil, turn on authentication.
+	ServeAll      bool   // If is false, path start with dot will not be served, that means a 404 error will be returned.
 }
-
-
